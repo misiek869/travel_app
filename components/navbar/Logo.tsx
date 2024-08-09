91a841c1859053ccb595455d083b0bf26807e14c
Michal Gawik <gawlik.zator@o2.pl>
2024-08-09 11:55:46 +0200

    crete body

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { FaRegCompass } from 'react-icons/fa'
+import Link from 'next/link'
+import { Button } from '../ui/button'
 
 const Logo = () => {
-	return <div></div>
+	return (
+		<Button size='icon' asChild>
+			<Link href='/'>
+				<FaRegCompass className='w-6 h-6' />
+			</Link>
+		</Button>
+	)
 }
 
 export default Logo
