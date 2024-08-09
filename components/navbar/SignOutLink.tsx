8bbffb03c47a394d22a38ee1c50b06211cd904af
Michal Gawik <gawlik.zator@o2.pl>
2024-08-09 13:18:18 +0200

    create component body

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+'use client'
+
+import { SignOutButton } from '@clerk/nextjs'
+import { useToast } from '../ui/use-toast'
 
 const SignOutLink = () => {
-	return <div></div>
+	const { toast } = useToast()
+	const handleLogout = () => {
+		toast({ description: 'You have been signed out.' })
+	}
+	return (
+		<SignOutButton redirectUrl='/'>
+			<button className='w-full text-left' onClick={handleLogout}>
+				Logout
+			</button>
+		</SignOutButton>
+	)
 }
 
 export default SignOutLink
