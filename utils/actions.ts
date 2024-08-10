38944eefca14ff58b73a7d7c94bfcb0b16f1f165
Michal Gawik <gawlik.zator@o2.pl>
2024-08-10 07:19:37 +0200

    refactor createProfileAction

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,5 +1,9 @@
 'use server'
 
+import db from './db'
+import { auth, clerkClient, currentUser } from '@clerk/nextjs/server'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
 import { profileSchema } from './schemas'
 
 export const createProfileAction = async (
@@ -7,11 +11,31 @@ export const createProfileAction = async (
 	formData: FormData
 ) => {
 	try {
+		const user = await currentUser()
+		if (!user) throw new Error('Please login to create a profile')
+
 		const rawData = Object.fromEntries(formData)
 		const validatedFields = profileSchema.parse(rawData)
 
-		return { message: 'Profile Created' }
+		await db.profile.create({
+			data: {
+				clerkId: user.id,
+				email: user.emailAddresses[0].emailAddress,
+				profileImage: user.imageUrl ?? '',
+				...validatedFields,
+			},
+		})
+
+		// connect clerk
+		await clerkClient.users.updateUserMetadata(user.id, {
+			privateMetadata: {
+				hasProfile: true,
+			},
+		})
 	} catch (error) {
-		return { message: 'there was an error...' }
+		return {
+			message: error instanceof Error ? error.message : 'An error occurred',
+		}
 	}
+	redirect('/')
 }
