885826cdc26472ba387efe41d0c5930f1024150d
Michal Gawik <gawlik.zator@o2.pl>
2024-08-10 06:18:10 +0200

    change message

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -2,8 +2,8 @@ import * as z from 'zod'
 import { ZodSchema } from 'zod'
 
 export const profileSchema = z.object({
-	firstName: z.string().min(2, { message: 'min length is' }),
+	firstName: z.string().min(2, { message: 'min length is 2 characters' }),
 	// lastName: z.string().max(5, { message: 'max length is 5' }),
-	lastName: z.string().min(2, { message: 'min length is' }),
-	username: z.string().min(2, { message: 'min length is' }),
+	lastName: z.string().min(2, { message: 'min length is 2 characters' }),
+	username: z.string().min(2, { message: 'min length is 2 characters' }),
 })
