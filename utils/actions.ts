2878ce5c1b5236ab6c563dd420e073f29a5a8a93
Michal Gawik <gawlik.zator@o2.pl>
2024-08-10 06:18:27 +0200

    remove console logs

--- utils/actions.ts
+++ utils/actions.ts
@@ -9,10 +9,9 @@ export const createProfileAction = async (
 	try {
 		const rawData = Object.fromEntries(formData)
 		const validatedFields = profileSchema.parse(rawData)
-		console.log(validatedFields)
+
 		return { message: 'Profile Created' }
 	} catch (error) {
-		console.log(error)
 		return { message: 'there was an error...' }
 	}
 }
