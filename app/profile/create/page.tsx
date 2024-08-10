49d96e5213819fb987dd3b54ce8da5456cebfab9
Michal Gawik <gawlik.zator@o2.pl>
2024-08-10 05:51:03 +0200

    refactor and add FormContainer

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,11 +1,12 @@
-import { Label } from '@/components/ui/label'
-import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
+import FormContainer from '@/components/form/FormContainer'
+import FormInput from '@/components/form/FormInput'
+import { SubmitButton } from '@/components/form/Buttons'
 
-const createProfileAction = async (formData: FormData) => {
+const createProfileAction = async (prevState: any, formData: FormData) => {
 	'use server'
 	const firstName = formData.get('firstName') as string
-	console.log(firstName)
+
+	return { message: 'Profile Created' }
 }
 
 const CreateProfilePage = () => {
@@ -13,11 +14,14 @@ const CreateProfilePage = () => {
 		<section>
 			<h1 className='text-2xl font-semibold mb-8 capitalize'>new user</h1>
 			<div className='border p-8 rounded-md max-w-lg'>
-				<form action={createProfileAction}>
-					<Button type='submit' size='lg'>
-						Create Profile
-					</Button>
-				</form>
+				<FormContainer action={createProfileAction}>
+					<div className='grid gap-4 mt-4 '>
+						<FormInput type='text' name='firstName' label='First Name' />
+						<FormInput type='text' name='lastName' label='Last Name' />
+						<FormInput type='text' name='username' label='Username' />
+					</div>
+					<SubmitButton text='Create Profile' className='mt-8' />
+				</FormContainer>
 			</div>
 		</section>
 	)
