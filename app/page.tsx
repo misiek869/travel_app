a9b46eeeb9041b2070aafa5fa14494b2755b6aa3
Michal Gawik <gawlik.zator@o2.pl>
2024-08-11 14:52:48 +0200

    add CategoriesList and PropertiesContainer

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
 import CategoriesList from '@/components/home/CategoriesList'
 import PropertiesContainer from '@/components/home/PropertiesContainer'
-import React from 'react'
 
-const HomePage = () => {
+const HomePage = ({
+	searchParams,
+}: {
+	searchParams: { category?: string; search?: string }
+}) => {
 	return (
 		<section>
-			<CategoriesList />
-			<PropertiesContainer />
+			<CategoriesList
+				category={searchParams.category}
+				search={searchParams.search}
+			/>
+			<PropertiesContainer
+				category={searchParams.category}
+				search={searchParams.search}
+			/>
 		</section>
 	)
 }
