f2bf029bd2ed8293c3af10075b6536a7e048c8d2
Michal Gawik <gawlik.zator@o2.pl>
2024-08-16 13:31:46 +0200

    add CardSignInButton when user isn't logged in

--- components/card/FavoriteToggleButton.tsx
+++ components/card/FavoriteToggleButton.tsx
@@ -1,7 +1,11 @@
 import { FaHeart } from 'react-icons/fa'
 import { Button } from '@/components/ui/button'
+import { CardSignInButton } from '../form/Buttons'
+import { auth } from '@clerk/nextjs/server'
 
 const FavoriteToggleButton = ({ propertyId }: { propertyId: string }) => {
+	const { userId } = auth()
+	if (!userId) return <CardSignInButton />
 	return (
 		<Button size='icon' variant='outline' className='p-2 cursor-pointer'>
 			<FaHeart />
