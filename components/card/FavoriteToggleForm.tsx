f5ca0c4c24309ec5f6bcd0eedbb92384b31cfc9f
Michal Gawik <gawlik.zator@o2.pl>
2024-08-16 14:02:02 +0200

    refactor

--- components/card/FavoriteToggleForm.tsx
+++ components/card/FavoriteToggleForm.tsx
@@ -1,11 +1,31 @@
-import { FaHeart } from 'react-icons/fa'
-import { Button } from '@/components/ui/button'
+'use client'
 
-const FavoriteToggleForm = ({ propertyId }: { propertyId: string }) => {
+import { usePathname } from 'next/navigation'
+import FormContainer from '../form/FormContainer'
+import { toggleFavoriteAction } from '@/utils/actions'
+import { CardSubmitButton } from '../form/Buttons'
+
+type FavoriteToggleFormProps = {
+	propertyId: string
+	favoriteId: string | null
+}
+
+const FavoriteToggleForm = ({
+	propertyId,
+	favoriteId,
+}: FavoriteToggleFormProps) => {
+	const pathname = usePathname()
+
+	const toggleAction = toggleFavoriteAction.bind(null, {
+		propertyId,
+		favoriteId,
+		pathname,
+	})
+	
 	return (
-		<Button size='icon' variant='outline' className='p-2 cursor-pointer'>
-			<FaHeart />
-		</Button>
+		<FormContainer action={toggleAction}>
+			<CardSubmitButton isFavorite={favoriteId ? true : false} />
+		</FormContainer>
 	)
 }
 
