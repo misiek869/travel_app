79b32722665ee6a36ee56f863195a5d648ad49f6
Michal Gawik <gawlik.zator@o2.pl>
2024-08-16 11:31:32 +0200

    add PropertyRating component

--- components/card/PropertyCard.tsx
+++ components/card/PropertyCard.tsx
@@ -26,7 +26,7 @@ const PropertyCard = ({ property }: { property: PropertyCardProps }) => {
 					<h3 className='text-sm font-semibold mt-1'>
 						{name.substring(0, 30)}
 					</h3>
-					{/* property rating */}
+					<PropertyRating inPage={false} propertyId={propertyId} />
 				</div>
 				<p className='text-sm mt-1 text-muted-foreground '>
 					{tagline.substring(0, 40)}
