a647e21c5c6e5408e36383dcbc46cc35b3b2bef5
Michal Gawik <gawlik.zator@o2.pl>
2024-08-17 05:36:33 +0200

    create loading page

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import EmptyList from '@/components/home/EmptyList'
+import PropertiesList from '@/components/home/PropertiesList'
+import { fetchFavorites } from '@/utils/actions'
 
-const FavoritesPage = () => {
-	return <div>favorites</div>
+const FavoritesPage = async () => {
+	const favorites = await fetchFavorites()
+
+	if (favorites.length === 0) {
+		return <EmptyList />
+	}
+
+	return <PropertiesList properties={favorites} />
 }
 
 export default FavoritesPage
