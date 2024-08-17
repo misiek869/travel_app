cc9b2050ba5552a093c8040212f25985e592d430
Michal Gawik <gawlik.zator@o2.pl>
2024-08-17 05:55:42 +0200

    add section

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,5 +1,7 @@
+import BreadCrumbs from '@/components/properties/BreadCrumbs'
 import { fetchPropertyDetails } from '@/utils/actions'
 import { redirect } from 'next/navigation'
+import FavoriteToggleButton from '@/components/card/FavoriteToggleButton'
 
 const PropertyDetailsPage = async ({ params }: { params: { id: string } }) => {
 	const property = await fetchPropertyDetails(params.id)
@@ -7,7 +9,18 @@ const PropertyDetailsPage = async ({ params }: { params: { id: string } }) => {
 	const { baths, bedrooms, beds, guests } = property
 	const details = { baths, bedrooms, beds, guests }
 
-	return <div>PropertyDetailsPage</div>
+	return (
+		<section>
+			<BreadCrumbs name={property.name} />
+			<header className='flex justify-between items-center mt-4'>
+				<h1 className='text-4xl font-bold capitalize'>{property.tagline}</h1>
+				<div className='flex items-center gap-x-4'>
+					{/* share button */}
+					<FavoriteToggleButton propertyId={property.id} />
+				</div>
+			</header>
+		</section>
+	)
 }
 
 export default PropertyDetailsPage
