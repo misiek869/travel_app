8facb6527be9de76faec5688f277304c0a7420a8
Michal Gawik <gawlik.zator@o2.pl>
2024-08-19 12:21:17 +0200

    create BookingWrapper logic

--- components/booking/BookingWrapper.tsx
+++ components/booking/BookingWrapper.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+'use client'
 
-const BookingWrapper = () => {
-	return <div></div>
+import { useProperty } from '@/utils/store'
+import { Booking } from '@/utils/types'
+import BookingCalendar from './BookingCalendar'
+import BookingContainer from './BookingContainer'
+import { useEffect } from 'react'
+
+type BookingWrapperProps = {
+	propertyId: string
+	price: number
+	bookings: Booking[]
+}
+
+const BookingWrapper = ({
+	propertyId,
+	price,
+	bookings,
+}: BookingWrapperProps) => {
+	useEffect(() => {
+		useProperty.setState({
+			propertyId,
+			price,
+			bookings,
+		})
+	}, [])
+
+	return (
+		<>
+			<BookingCalendar />
+			<BookingContainer />
+		</>
+	)
 }
 
 export default BookingWrapper
