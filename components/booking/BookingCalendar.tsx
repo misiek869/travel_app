617b25ad0f4cbd9d41dbc87c6fbee7b4dd7b7e14
Michal Gawik <gawlik.zator@o2.pl>
2024-08-19 12:52:56 +0200

    add imports

--- components/booking/BookingCalendar.tsx
+++ components/booking/BookingCalendar.tsx
@@ -1,4 +1,16 @@
-import React from 'react'
+'use client'
+import { Calendar } from '@/components/ui/calendar'
+import { useEffect, useState } from 'react'
+import { useToast } from '@/components/ui/use-toast'
+import { DateRange } from 'react-day-picker'
+import { useProperty } from '@/utils/store'
+
+import {
+	// generateDisabledDates,
+	// generateDateRange,
+	defaultSelected,
+	// generateBlockedPeriods,
+} from '@/utils/calendar'
 
 const BookingCalendar = () => {
 	return <div></div>
