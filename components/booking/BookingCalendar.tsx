ea749a7a3cf15a4730ed8e1c2e729e8bfcc9bfd7
Michal Gawik <gawlik.zator@o2.pl>
2024-08-19 13:04:22 +0200

    create BookingCalendar body

--- components/booking/BookingCalendar.tsx
+++ components/booking/BookingCalendar.tsx
@@ -13,7 +13,23 @@ import {
 } from '@/utils/calendar'
 
 const BookingCalendar = () => {
-	return <div></div>
+	const currentDate = new Date()
+
+	const [range, setRange] = useState<DateRange | undefined>(defaultSelected)
+
+	useEffect(() => {
+		useProperty.setState({ range })
+	}, [range])
+
+	return (
+		<Calendar
+			mode='range'
+			defaultMonth={currentDate}
+			selected={range}
+			onSelect={setRange}
+			className='mb-4'
+		/>
+	)
 }
 
 export default BookingCalendar
