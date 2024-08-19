52e3794e6947d11a1522da895f3e20d3774ccaa4
Michal Gawik <gawlik.zator@o2.pl>
2024-08-19 13:18:57 +0200

    create calculateDaysBetween

--- utils/calendar.ts
+++ utils/calendar.ts
@@ -5,3 +5,19 @@ export const defaultSelected: DateRange = {
 	from: undefined,
 	to: undefined,
 }
+
+export function calculateDaysBetween({
+	checkIn,
+	checkOut,
+}: {
+	checkIn: Date
+	checkOut: Date
+}) {
+	// Calculate the difference in milliseconds
+	const diffInMs = Math.abs(checkOut.getTime() - checkIn.getTime())
+
+	// Convert the difference in milliseconds to days
+	const diffInDays = diffInMs / (1000 * 60 * 60 * 24)
+
+	return diffInDays
+}
