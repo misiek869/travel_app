856323c1fd99e05d3add780dcd37893acd6e2aed
Michal Gawik <gawlik.zator@o2.pl>
2024-08-21 12:45:18 +0200

    create component body

--- components/admin/StatsCard.tsx
+++ components/admin/StatsCard.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { Card, CardHeader } from '@/components/ui/card'
 
-const StatsCard = () => {
-	return <div></div>
+type StatsCardsProps = {
+	title: string
+	value: number
 }
 
+const StatsCard = ({ title, value }: StatsCardsProps) => {
+	return (
+		<Card className='bg-muted'>
+			<CardHeader className='flex flex-row justify-between items-center'>
+				<h3 className='capitalize text-3xl font-bold'>{title}</h3>
+				<span className='text-primary text-5xl font-extrabold'>{value}</span>
+			</CardHeader>
+		</Card>
+	)
+}
 export default StatsCard
