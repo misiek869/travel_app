8c3e9de2345f8c3d186c2c95122b6dfe7609bf66
Michal Gawik <gawlik.zator@o2.pl>
2024-08-21 12:55:53 +0200

    create component body

--- components/admin/ChartsContainer.tsx
+++ components/admin/ChartsContainer.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import { fetchChartsData } from '@/utils/actions'
+import Chart from './Chart'
 
-const ChartsContainer = () => {
-	return <div></div>
+const ChartsContainer = async () => {
+	const bookings = await fetchChartsData()
+	if (bookings.length < 1) return null
+
+	return <Chart data={bookings} />
 }
 
 export default ChartsContainer
