c2a4199497b5c543b4271adddd39e79b19b6472e
Michal Gawik <gawlik.zator@o2.pl>
2024-08-21 12:18:35 +0200

    nothing important

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import ChartsContainer from '@/components/admin/ChartsContainer'
+import StatsContainer from '@/components/admin/StatsContainer'
+import {
+	ChartsLoadingContainer,
+	StatsLoadingContainer,
+} from '@/components/admin/Loading'
+import { Suspense } from 'react'
 
-const AdminPage = () => {
-	return <div>admin</div>
+const AdminPage = async () => {
+	return (
+		<>
+			<Suspense fallback={<StatsLoadingContainer />}>
+				<StatsContainer />
+			</Suspense>
+			<Suspense fallback={<ChartsLoadingContainer />}>
+				<ChartsContainer />
+			</Suspense>
+		</>
+	)
 }
 
 export default AdminPage
