5160f49af0b9b8949f8b9bafeba68254b6003cf4
Michal Gawik <gawlik.zator@o2.pl>
2024-08-21 10:44:33 +0200

    return LoadingTable

--- app/reservations/loading.tsx
+++ app/reservations/loading.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+'use client'
 
+import LoadingTable from '@/components/booking/LoadingTable'
 const loading = () => {
-	return <div></div>
+	return <LoadingTable />
 }
 
 export default loading
